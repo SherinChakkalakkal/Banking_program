60a0d705057bfbbc13292c8e86d016f069f5a10a
Sherin Chakkalakkal <sherinchakkalakkal@Sherins-MBP.hitronhub.home>
2021-06-06 11:29:38 -0500

    new_feature_second

--- second.py
+++ second.py
@@ -1,3 +1,4 @@
 print("Hello Nithi and Charu")
 
 print("New feature branch details")
+print("New feature branch details follows")
--- test.py
+++ test.py
@@ -2,3 +2,5 @@ print("hello git")
 print("second change")
 
 print("new feature branch is updated")
+
+print("new feature branch codes")
